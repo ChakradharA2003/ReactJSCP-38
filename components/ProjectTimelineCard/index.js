16936fa283d8cfd142dfe693a254a73cdd84e761
ChakradharA2003 <144037702+ChakradharA2003@users.noreply.github.com>
2024-05-11 16:39:48 +0530

    Update index.js

--- components/ProjectTimelineCard/index.js
+++ components/ProjectTimelineCard/index.js
@@ -1,4 +1,5 @@
 // Write your code here
+import {AiFillCalendar} from 'react-icons/ai'
 import './index.css'
 
 const ProjectTimelineCard = props => {
@@ -7,7 +8,18 @@ const ProjectTimelineCard = props => {
   console.log(details)
   return (
     <div className="project-container">
-      <img src={imageUrl} alt={projectTitle} className="project-image" />
+      <img src={imageUrl} alt="project" className="project-image" />
+      <div className="head-container">
+        <h1 className="project-title">{projectTitle}</h1>
+        <div className="durations-container">
+          <AiFillCalendar />
+          <p className="duration-days">{duration}</p>
+        </div>
+      </div>
+      <p className="description">{description}</p>
+      <a href={projectUrl} className="project-link">
+        Visit
+      </a>
     </div>
   )
 }
